039792c55b5ed5a3aaf8fee840c1bad7a0906422
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2025-04-01 19:49:37 +0300

    feat: add AuthModel and AuthController

---
+++ GrademgrBackend/Models/AuthModels.cs
@@ -0,0 +1,25 @@
+public class RegisterRequest
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string FullName { get; set; }
+    public UserRole Role { get; set; }
+}
+
+public class LoginRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class RegisterResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+}
+
+public class LoginResponse
+{
+    public string Token { get; set; }
+}
