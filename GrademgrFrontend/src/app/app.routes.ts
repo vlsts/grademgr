ee069db75deb25f3a73b2912c74e42a9fc291e47
Vlad-Stefan Sas <me@vlad.pm>
2025-04-01 19:37:18 +0300

    feat: (GRADEMGR-43) router outlet and routes

--- GrademgrFrontend/src/app/app.routes.ts
+++ GrademgrFrontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
