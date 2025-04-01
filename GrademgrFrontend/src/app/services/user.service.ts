0490d0292a59286e5820b6b95f9a51b251504cef
Vlad-Stefan Sas <me@vlad.pm>
2025-04-01 19:38:35 +0300

    feat: (GRADEMGR-43) user service

---
+++ GrademgrFrontend/src/app/services/user.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = 'http://localhost:5052/api/Auth';
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users`, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`, user);
+  }
+
+  deleteUser(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/users/${id}`);
+  }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, { username, password });
+  }
+
+  register(user: { username: string; email: string; password: string; fullName: string; role: number }): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, {
+      ...user,
+      role: Number(user.role),
+    });
+  }
+}
