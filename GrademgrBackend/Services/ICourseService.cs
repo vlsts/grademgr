50854fdc3c821935ff1d97a66cb028e525d44968
tdennis <teglasdennis@yahoo.com>
2025-04-08 16:08:04 +0300

    feat: (GRADEMGR-36) added bulk upload for grades endpoint

--- GrademgrBackend/Services/ICourseService.cs
+++ GrademgrBackend/Services/ICourseService.cs
@@ -15,7 +15,8 @@ public interface ICourseService
     Task<List<Course>> GetStudentCoursesAsync(string studentEmail);
 
     Task<bool> AddGradeToCourseAsync(string courseId, string studentEmail, GradeRequest request, string teacherEmail);
-Task<List<GradeWithStudentInfo>> GetGradesForCourseAsync(string courseId, string teacherEmail);
+    Task<List<bool>> AddBulkGradesToCourseAsync(string courseId, List<AddGradeRequest> requests, string teacherEmail);
+    Task<List<GradeWithStudentInfo>> GetGradesForCourseAsync(string courseId, string teacherEmail);
     Task<bool> DeleteGradeFromCourseAsync(string courseId, string gradeId, string teacherEmail);
     Task<List<GradeDetailsDto>> GetGradesForStudentAsync(string studentEmail);
     Task<List<Grade>> GetGradesForCourseAsStudentAsync(string courseId, string studentEmail);
